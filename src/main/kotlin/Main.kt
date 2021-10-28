a05b294df611b829428af8c2ae4dca5ed4234fbe
FranciscoRibeiro <kikoribeiro95@gmail.com>
2021-10-28 17:06:54 +0100

    First version: filepath and line number as input and returns column numbers corresponding to the beginning and ending of each AST node found in the line in question

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,19 @@
-fun main(args: Array<String>) {
+import com.github.javaparser.StaticJavaParser
+import com.github.javaparser.ast.Node
+import java.io.File
 
+fun truncate(filePath: String, lineNr: Int) {
+    val ast = StaticJavaParser.parse(File(filePath))
+    val nodes = ast.findAll(Node::class.java, { it.begin.get().line == lineNr && it.end.get().line == lineNr })
+//    nodes.forEach { println(it.javaClass.simpleName); println(it) }
+    val colNrs = nodes.flatMap { listOf(it.range.get().begin.column, it.range.get().end.column) }.distinct()
+//    nodes.forEach { print("$it -> "); print("${it.range.get().begin.column} "); println(it.range.get().end.column) }
+    colNrs.forEach { print("$it ") }
+}
+
+fun main(args: Array<String>) {
+    val filePath = args[0]
+    val lineNr = args[1].toInt()
+
+    truncate(filePath, lineNr)
 }
