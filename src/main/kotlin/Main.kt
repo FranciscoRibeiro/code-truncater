349d5b2142f68ea6af78a12b177b2b38c8209506
FranciscoRibeiro <kikoribeiro95@gmail.com>
2021-11-08 23:19:10 +0000

    Add new execution mode '--is-str' that tells if a column in a certain line is inside a string. Helpful to know if the tools' heuristics will have trouble indicating that column as a truncate possibility

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 import com.github.javaparser.ParseProblemException
 import com.github.javaparser.StaticJavaParser
+import com.github.javaparser.ast.CompilationUnit
 import com.github.javaparser.ast.Node
 import com.github.javaparser.ast.expr.SimpleName
 import com.github.javaparser.ast.expr.StringLiteralExpr
@@ -26,14 +27,30 @@ fun inLine(node: Node, lineNr: Int): Boolean {
     return beginPos.line == lineNr && endPos.line == lineNr
 }
 
-fun truncate(file: File, lineNr: Int): List<Int> {
-    val ast = try {
+fun hasColNr(node: Node, colNr: Int): Boolean {
+    val beginPos = node.begin.orElse(null) ?: return false
+    val endPos = node.end.orElse(null) ?: return false
+    return beginPos.column <= colNr && endPos.column >= colNr
+}
+
+fun parse(file: File): CompilationUnit? {
+    return try {
         StaticJavaParser.parse(file)
     } catch (e: ParseProblemException) {
-        return emptyList()
+        null
     }
+}
+
+fun truncate(file: File, lineNr: Int): List<Int> {
+    val ast = parse(file) ?: return emptyList()
     val nodes = ast.findAll(Node::class.java, { inLine(it, lineNr) })
     return nodes.flatMap { columns(it) }.distinct()
+}
+
+fun bugInStr(file: File, lineNr: Int, colNr: Int): Boolean {
+    val ast = parse(file) ?: return false
+    val strings = ast.findAll(StringLiteralExpr::class.java, { inLine(it, lineNr) && hasColNr(it, colNr) })
+    return strings.isNotEmpty()
 }
 
 fun main(args: Array<String>) {
@@ -45,6 +62,13 @@ fun main(args: Array<String>) {
             .map { it.split(" ") }
             .map { Triple(it[0], it[1], truncate(File(prefix, it[0]), it[1].toInt())) }
             .forEach { println("${it.first} ${it.second} ${it.third.joinToString(",")}") }
+    } else if(args[0] == "--is-str") {
+        val dataFile = args[1]
+        val prefix = args[2]
+
+        File(dataFile).readLines()
+            .map { it.split(" ") }
+            .forEach { if(bugInStr(File(prefix, it[0]), it[1].toInt(), it[2].toInt())) println(it[0]) }
     } else {
         val filePath = args[0]
         val lineNr = args[1].toInt()
